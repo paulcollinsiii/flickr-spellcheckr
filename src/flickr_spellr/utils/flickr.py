f1782d00feec7e3daf8c5c3e0af78b9b614f2484
Paul Collins <paul.collins.iii@gmail.com>
2012-03-08 20:18:34 -0800

    Initial version of the flickr spell checker

---
+++ src/flickr_spellr/utils/flickr.py
@@ -0,0 +1,116 @@
+'''Module to handle all the query nastiness and pagingation with flickr
+'''
+
+import flickrapi
+import logging
+from flickr_spellr.utils.exceptions import InvalidState
+import time
+
+APIKEY = 'b60fd0ba95f8c583d8ef513d060c68e8'
+APISECRET = '9479730e8bc2c49a'
+
+class SimplePhoto(object):
+    '''Easier to deal with Photo object from flickr
+    '''
+
+    def __init__(self, title, description, photo_id):
+        '''
+        
+        :param title: Text of the photo title
+        :param description: Text for the description
+        :param photo_id: The photo ID
+        '''
+
+        self.title = title
+        self.description = description
+        self.photo_id = photo_id
+
+
+class Flickr(object):
+    LOGGER = logging.getLogger('flickr.Flickr')
+
+    def __init__(self):
+        '''Handle querying and iterating over resultant photo data
+        
+        :ivar logged_in: Boolean for if we're logged into flickr
+        :ivar _flickr: Instance of :obj:`flickrapi`
+        '''
+
+        self.logged_in = False
+        self._flickr = flickrapi.FlickrAPI(APIKEY, APISECRET, cache=True)
+
+    def login(self):
+        '''Setup the :attr:`flickr` object and perform the login to flickr
+        
+        It is safe to call this function several times. Subsequent calls have
+        no effect.
+        
+        :keyword get_token: If get_token_part_one succeeds, get the frob
+        :return: True if fully logged in callable to finish login otherwise
+        '''
+        def finish_login():
+            self._flickr.get_token_part_two((token, frob))
+            self.logged_in = True
+            return True
+        if self.logged_in:
+            return True
+        (token, frob) = self._flickr.get_token_part_one(perms='write')
+        if not token:
+            return finish_login
+        finish_login()
+
+    def photos_iter(self, date_from, date_to=None):
+        '''Return an iterator over flickr photos and handle all pagination
+        
+        The search will only return photos owned by the logged in user.
+        The photos objects returned will have their description attached.
+        
+        :param date_from: The min date the photo was taken on
+        :keyword date_to: The max date the photo was taken on. Default: now
+        '''
+
+        def get_photos_element(resp):
+            photos = resp.getchildren()
+            if len(photos) != 1:
+                raise InvalidState('Flickr XML response in unexpected format')
+            photos = photos[0]
+            for key in ('page', 'pages', 'total'):
+                if key not in photos.attrib:
+                    raise InvalidState('Flickr XML response in unexpected '
+                                       'format')
+            return photos
+
+        def simplephoto_iter(photos):
+            for photo in photos.getchildren():
+                for key in ('title', 'id'):
+                    if key not in photo.attrib:
+                        raise InvalidState('Flickr XML response in unexpected '
+                                           'format')
+                children = photo.getchildren()
+                if len(children) != 1:
+                    raise InvalidState('Flickr XML response in unexpected '
+                                       'format')
+                yield SimplePhoto(title=photo.attrib['title'],
+                                  description=children[0].text,
+                                  photo_id=photo.attrib['id'])
+
+        self.LOGGER.debug('photos_search : %s %s', date_from, date_to)
+        assert self.logged_in, 'Must be logged in to flickr to search own photos'
+        #TODO: Convert datetimes to unix timestamps
+        search_args = {'min_taken_date': time.mktime(date_from.timetuple()),
+                       'user_id': 'me',
+                       'extras': 'description',
+                       'per_page': 5}
+        if date_to is not None:
+            search_args['max_taken_date'] = time.mktime(date_to.timetuple())
+        resp = self._flickr.photos_search(**search_args)
+        for simplephoto in simplephoto_iter(get_photos_element(resp)):
+            yield simplephoto
+        #TODO: Renable multi page support
+#        if photos.attrib['pages'] != '1':
+#            self.LOGGER.info('Flickr image search returned %s pages of '
+#                             'results...', photos.attrib['pages'])
+#        for idx in xrange(2,int(photos.attrib['pages']) + 1):
+#            resp = self._flickr.photos_search(page=idx, **kwargs)
+#            photos = get_photos_element(resp)
+
